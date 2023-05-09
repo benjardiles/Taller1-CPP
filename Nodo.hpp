764d6b8462c50e5b24198c4cf8e374a887a2c279
Benjamin ardiles <89605478+benjardiles@users.noreply.github.com>
2023-05-09 03:47:40 -0400

    Update Nodo.hpp

--- Nodo.hpp
+++ Nodo.hpp
@@ -1,23 +1,64 @@
-class Nodo{
-    private:
-        Nodo*Up;
-        Nodo*Left;
-        int J;
-        int I;  
-        Nodo *siguiente;
-    public:
-        Nodo(int I,int );
-        void setUp(Nodo*N);
-        Nodo*N getUp(); 
-        void setI(int I);   
-        void setJ(int J);  
-         
-        Nodo get I(){
-            return I;
-        }
-        Nodo void get J(){
-            return J;
-        }	
+class Nodo {
+private:
+    int Nota;
+    int J;
+    int I;
+    Nodo* up;
+    Nodo* left;
+    Nodo* siguiente;
+   
+public:
+    Nodo(int Nota, int J, int I) {
+        this->Nota = Nota;
+        up = nullptr;
+        left = nullptr;
+        siguiente = nullptr;
+    }
+    int getNota() {
+        return Nota;
+    }
+    int getJ() {
+        return J;
+    }
+    int getI() {
+        return I;
+    }
+    Nodo* getUp() {
+        return up;
+    }
+    Nodo* getLeft() {
+        return left;
+    }
+    Nodo* getSiguiente() {
+        return siguiente;
+    }
+    Nodo setSiguiente(Nodo* siguiente) {
+        this->siguiente = siguiente;
+    }
+    Nodo setUp(Nodo* up) {
+        this->up = up;
+    }
+    Nodo setLeft(Nodo* left) {
+        this->left = left;
+    }  
 
+    Nodo setNota(int Nota) {
+        this->Nota = Nota;
+    }
+    Nodo setJ(int J) {
+        this->J = J;
+    }
+    Nodo setI(int I) {
+        this->I = I;
+    }
     
 };
+void agregarNodo(Nodo* nodo, int Nota, int J, int I) {
+    if (nodo->getSiguiente() == nullptr) {
+        Nodo* nuevo = new Nodo(Nota, J, I);
+        nodo->setSiguiente(nuevo);
+        return;
+    }else {
+        agregarNodo(nodo->getSiguiente(), Nota, J, I);
+    }
+}
