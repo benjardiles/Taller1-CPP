48834632c0002ef02be0ee9042e3abd03c9479ce
Benjamin ardiles <89605478+benjardiles@users.noreply.github.com>
2023-05-08 20:54:10 -0400

    Add files via upload

---
+++ Nodo.hpp
@@ -0,0 +1,23 @@
+class Nodo{
+    private:
+        Nodo*Up;
+        Nodo*Left;
+        int J;
+        int I;  
+        Nodo *siguiente;
+    public:
+        Nodo(int I,int );
+        void setUp(Nodo*N);
+        Nodo*N getUp(); 
+        void setI(int I);   
+        void setJ(int J);  
+         
+        Nodo get I(){
+            return I;
+        }
+        Nodo void get J(){
+            return J;
+        }	
+
+    
+};
---
+++ Taller.cpp
@@ -0,0 +1,3 @@
+#include<<iostream>
+#include<Nodo.hpp>
+using namespace std;
